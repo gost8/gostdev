e4c38e6c256aef2627613bbcf2d533803dadb643
Yuri Karamani <y.karamani@gmail.com>
2015-09-19 12:59:35 +0400

    cli commands added

--- src/gostdev/gostdev.go
+++ src/gostdev/gostdev.go
@@ -10,6 +10,36 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "gostdev"
 	app.Usage = "Golang code generation tool"
+	app.Author = "Yuri Karamani <y.karamani@gmail.com>"
+	app.Commands = []cli.Command{
+		{
+			Name: "create",
+			Subcommands: []cli.Command{
+      			{
+ 					Name:  "routes",
+ 					Usage: "create routes for web application",
+					Action: func(c *cli.Context) {
+						fmt.Println("create routes")
+					},
+				},
+     			{
+ 					Name:  "models",
+ 					Usage: "create models for web application",
+					Action: func(c *cli.Context) {
+						fmt.Println("create models")
+					},
+				},				
+     			{
+ 					Name:  "webclient",
+ 					Usage: "create client package for web application",
+					Action: func(c *cli.Context) {
+						fmt.Println("create webclient")
+					},
+				},
+			},
+		},
+	}
+	
 	app.Action = func(c *cli.Context) {
 		fmt.Println("Cool tool!")
 	}
