35007f2098b48a5eeaf63e4d5a31284780ec44db
Yuri Karamani <y.karamani@gmail.com>
2015-09-21 08:26:28 +0400

    yaml parser included

--- src/gostdev/gostdev.go
+++ src/gostdev/gostdev.go
@@ -3,8 +3,25 @@ package main
 import (
 	"fmt"
 	"github.com/codegangsta/cli"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"os"
 )
+
+type Field struct {
+	Name string
+	Type string
+}
+
+type Fields []Field
+
+type Entity struct {
+	Fields Fields
+}
+
+type Schema struct{
+	Entity Entity
+}
 
 func main() {
 	app := cli.NewApp()
@@ -19,6 +36,20 @@ func main() {
  					Name:  "routes",
  					Usage: "create routes for web application",
 					Action: func(c *cli.Context) {
+						data, err := ioutil.ReadFile("/home/y2k/projects/gostdev/test/fixtures/schema1/schema.yaml")
+						if err != nil {
+							fmt.Printf("error: %v", err)
+							panic(err)
+						}
+						
+						fmt.Print(string(data))
+						entity := Schema{}
+						err = yaml.Unmarshal(data, &entity)
+						if err != nil {
+							fmt.Printf("error: %v", err)
+							panic(err)
+						}
+						fmt.Printf("--- t:\n%v\n\n", entity)
 						fmt.Println("create routes")
 					},
 				},
